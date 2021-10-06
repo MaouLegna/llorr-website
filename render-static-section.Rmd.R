8eef3ed2443e1964cbf213e305452c1dfae3621b
MaouLegna <valentinovazzoler@gmail.com>
2021-10-06 07:23:36 +0200

    update 2021-10-06

--- render-static-section.Rmd.R
+++ render-static-section.Rmd.R
@@ -1,14 +1,9 @@
-rmarkdown::render_site(
-  input = "static/mu.Rmd",
-  envir = new.env()
-)
+# rmarkdown::render_site( )
 
-rmarkdown::render(
-  input = "static/meta.Rmd",
-  envir = new.env()
-)
+# rmarkdown::render(
+#   input = "static/meta.Rmd",
+#   envir = new.env()
+# )
 
-rmarkdown::render(
-  input = "index.Rmd",
-  envir = new.env()
-)
+rmarkdown::render("static/mu.Rmd", "html_document", envir = new.env())
+rmarkdown::render("static/meta.Rmd", "html_document", envir = new.env())
