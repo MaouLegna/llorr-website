1d9e018cae8b1d0fc6c877cc64a6710cd1b8fb0a
MaouLegna <valentinovazzoler@gmail.com>
2021-10-08 09:44:37 +0200

    update 2021-10-08 fix v1

--- render-static-section.Rmd.R
+++ render-static-section.Rmd.R
@@ -1,6 +1,3 @@
-# rmarkdown::render("static/mu.Rmd", "html_document")
-rmarkdown::render("static/mu.Rmd", envir = new.env())
-# rmarkdown::render("static/meta.Rmd", "html_document")
-rmarkdown::render("static/meta.Rmd", envir = new.env())
-
+rmarkdown::render("static/mu.Rmd", "html_document")
+rmarkdown::render("static/meta.Rmd", "html_document")
 rmarkdown::render_site(encoding = 'UTF-8')
