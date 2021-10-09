10f0b6544ee7c8957657a3323dd6e3170d1fc422
MaouLegna <valentinovazzoler@gmail.com>
2021-10-09 08:41:00 +0200

    update 2021-10-09

--- render-static-section.Rmd.R
+++ render-static-section.Rmd.R
@@ -2,6 +2,7 @@
 # rmarkdown::render("static/meta.Rmd", "html_document")
 knit_with_parameters("~/R/llorr-website/static/mu.Rmd")
 knit_with_parameters("~/R/llorr-website/static/meta.Rmd")
+
 rmarkdown::render_site(encoding = 'UTF-8')
 
 
