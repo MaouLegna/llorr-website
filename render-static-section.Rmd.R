c5fc124e175712aa8bc05212e6cb8006776a2895
MaouLegna <valentinovazzoler@gmail.com>
2021-10-13 19:47:25 +0200

    add meta-report-27

--- render-static-section.Rmd.R
+++ render-static-section.Rmd.R
@@ -1,11 +1,14 @@
-rmarkdown::render("static/mu.Rmd")
-rmarkdown::render("static/meta.Rmd")
+# list.files(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static"))
 
-# rmarkdown::render("static/meta.Rmd", "html_document")
-# knit_with_parameters("~/R/llorr-website/static/mu.Rmd")
-# knit_with_parameters("~/R/llorr-website/static/meta.Rmd")
-
-rmarkdown::render("index.Rmd")
-rmarkdown::render_site(encoding = 'UTF-8')
-
-
+# if ( Sys.time() > as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(6) &
+#      Sys.time() < as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(7) ) {
+#   
+#   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"))
+#   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"))
+#   
+#   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","index.Rmd"))
+#   rmarkdown::render_site(encoding = 'UTF-8')
+#   
+#   # knit_with_parameters("~/R/llorr-website/static/mu.Rmd")
+#   
+# }
