eec506e0e875e77bc2ce9c304e1196bc8d6de5cf
MaouLegna <valentinovazzoler@gmail.com>
2021-10-14 06:52:21 +0200

    update 2021-10-14

--- render-static-section.Rmd.R
+++ render-static-section.Rmd.R
@@ -1,14 +1,16 @@
 # list.files(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static"))
 
-# if ( Sys.time() > as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(6) &
-#      Sys.time() < as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(7) ) {
-#   
-#   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"))
-#   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"))
-#   
-#   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","index.Rmd"))
-#   rmarkdown::render_site(encoding = 'UTF-8')
-#   
-#   # knit_with_parameters("~/R/llorr-website/static/mu.Rmd")
-#   
-# }
+if ( Sys.time() > as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(6) &
+     Sys.time() < as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(7) ) {
+
+  rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"))
+  rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"))
+
+  rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","index.Rmd"))
+  rmarkdown::render_site(
+    input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website"),
+    encoding = 'UTF-8')
+
+  # knit_with_parameters("~/R/llorr-website/static/mu.Rmd")
+
+}
