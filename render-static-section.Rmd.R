6c2ea6a10950f81eb2d5aaae7b01bec3b3403b6c
MaouLegna <valentinovazzoler@gmail.com>
2021-10-15 06:52:07 +0200

    update 2021-10-15

--- render-static-section.Rmd.R
+++ render-static-section.Rmd.R
@@ -3,6 +3,12 @@
 if ( Sys.time() > as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(6) &
      Sys.time() < as.POSIXct(format(Sys.Date(), format = "%Y-%m-%d %H:%M:%OS"), tz = "CET") + lubridate::hours(7) ) {
 
+  fwrite(LoR.Account,  file.path("C:","LlorR","data","raw", "LoR_ACCOUNT.csv"  ),quote = TRUE )  
+  fwrite(LoR.Deck,     file.path("C:","LlorR","data","raw","LoR_DECK.csv"))
+  fwrite(LoR.Match.DT, file.path("C:","LlorR","data","raw", "LoR_MatchDT.csv") )
+  fwrite(LoR.Match.DT[, .(match_key, server, status)], file.path("C:","LlorR","data","raw", "LoR_HISTORY.csv") )
+  rtweet::write_as_csv(LoR.Twitter, file.path("C:","LlorR","data","raw", "LoR_Twitter.csv" ), prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+  
   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"))
   rmarkdown::render(file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"))
 
