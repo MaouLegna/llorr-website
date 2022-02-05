4c18847d5caac802d28cb5afebab589b60f3fb97
MaouLegna <valentinovazzoler@gmail.com>
2022-02-05 08:32:34 +0100

    add seasonal-04

--- render_static.R
+++ render_static.R
@@ -1,5 +1,8 @@
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd")  )
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"))
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","index.Rmd")        )
-rmarkdown::render_site(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website"),
-                       encoding = 'UTF-8')
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"),
+                  encoding = 'UTF-8')
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"),
+                  encoding = 'UTF-8')
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","index.Rmd",
+                                    encoding = 'UTF-8')        )
+rmarkdown::render_site(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website"), encoding = 'UTF-8')
+# rmarkdown::render_site(encoding = 'UTF-8')
