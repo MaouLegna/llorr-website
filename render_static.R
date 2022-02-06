867c20daea73e390d6f6c0f85863f1fbba2755f3
MaouLegna <valentinovazzoler@gmail.com>
2022-02-06 20:17:33 +0100

    update 2022-02-06 20:17

--- render_static.R
+++ render_static.R
@@ -1,8 +1,9 @@
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"),
-                  encoding = 'UTF-8')
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"),
-                  encoding = 'UTF-8')
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","index.Rmd",
-                                    encoding = 'UTF-8')        )
-rmarkdown::render_site(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website"), encoding = 'UTF-8')
-# rmarkdown::render_site(encoding = 'UTF-8')
+# Sections
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"), encoding = 'UTF-8')
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"), encoding = 'UTF-8')
+# rmarkdown::render_site('C:/Users/Valentino Vazzoler/Documents/R/llorr-website/report.Rmd',  encoding = 'UTF-8');
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","index.Rmd"), encoding = 'UTF-8')
+
+## Site
+# rmarkdown::render_site(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website"), encoding = 'UTF-8')
+rmarkdown::render_site(encoding = 'UTF-8')
