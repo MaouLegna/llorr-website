f12483cbe721cfd882fa3748e233fc369a518704
MaouLegna <valentinovazzoler@gmail.com>
2022-02-18 03:32:36 +0100

    update 2022-02-18

--- render_static.R
+++ render_static.R
@@ -1,14 +1,16 @@
 naptime::naptime( lubridate::as_datetime( lubridate::today(tzone = "CET")+lubridate::days(1),tz = "CET") )
+
 source(file.path("C:","LlorR","scripts","dataIO","lor_deck_v3.R"))
 data.table::fwrite(LoR.Deck, file.path("C:", "LlorR", "data", "raw", "LoR_DECK.csv"))
 
+tictoc::tic()
 # Sections
 # rmarkdown::render('C:/Users/Valentino Vazzoler/Documents/R/llorr-website/_report/meta-report-044/meta-report-044.Rmd',  encoding = 'UTF-8')
 rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"), encoding = 'UTF-8')
 rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"), encoding = 'UTF-8')
-# rmarkdown::render_site('C:/Users/Valentino Vazzoler/Documents/R/llorr-website/report.Rmd',  encoding = 'UTF-8');
-
+# Site
 rmarkdown::render_site('C:/Users/Valentino Vazzoler/Documents/R/llorr-website/index.Rmd',  encoding = 'UTF-8');
-## Site
-# rmarkdown::render_site(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website"), encoding = 'UTF-8')
 rmarkdown::render_site(encoding = 'UTF-8')
+
+tictoc::toc()
+Sys.time()
