2de0f1c87557d3d1714e122b99ed272861f25539
MaouLegna <valentino.vazzoler.86@gmail.com>
2022-03-30 05:01:24 +0200

    add-meta-report-51

--- render_static.R
+++ render_static.R
@@ -10,8 +10,8 @@ while (TRUE) {
   source(file.path("C:","LlorR","scripts","dataIO","lor_deck_v3.R"), encoding = "UTF-8", local = TRUE)
   data.table::fwrite(LoR.Deck, file.path("C:", "LlorR", "data", "raw", "LoR_DECK.csv"))
   
-  start <- Sys.time()
-  tictoc::tic()
+  start   <- Sys.time()
+  glue::glue("Knit - Start - {start}") |> message()
   
   # Update pages
   
@@ -20,11 +20,8 @@ while (TRUE) {
   rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"), encoding = 'UTF-8')
   rmarkdown::render_site(encoding = 'UTF-8')
   
-  tictoc::toc()
-  end <- Sys.time()
-  
-  message(start)
-  message(end)
+  end     <- Sys.time()
+  glue::glue("Knit - End   - {end} / Duration: {round(as.numeric(lubridate::as.duration(end-start), 'seconds'),2)}s") |> message()
   
   giorno <- Sys.time() |> lubridate::floor_date("days") + lubridate::hours(27)
   
