65d3a5b750b16733e554d5082ff5e3937505d21e
MaouLegna <valentino.vazzoler.86@gmail.com>
2022-04-14 05:47:57 +0200

    update 2022-04-14 05:47

--- render_static.R
+++ render_static.R
@@ -1,7 +1,5 @@
 while (TRUE) {
   
-  source(file.path("C:","LlorR","scripts","dataIO","lor_deck_v3.R"), encoding = "UTF-8", local = TRUE)
-  data.table::fwrite(LoR.Deck, file.path("C:", "LlorR", "data", "raw", "LoR_DECK.csv"))
   
   if (!exists("giorno")) {
     giorno <- Sys.time() |> lubridate::floor_date("days") + lubridate::hours(27)
@@ -12,6 +10,8 @@ while (TRUE) {
   
   # Update the decks
   
+  source(file.path("C:","LlorR","scripts","dataIO","lor_deck_v3.R"), encoding = "UTF-8", local = TRUE)
+  data.table::fwrite(LoR.Deck, file.path("C:", "LlorR", "data", "raw", "LoR_DECK.csv"))
   
   start   <- Sys.time()
   glue::glue("Knit - Start - {start}") |> message()
