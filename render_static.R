d066e52d518f158cc563ea47259727a19a2010e4
MaouLegna <valentino.vazzoler.86@gmail.com>
2022-05-15 08:58:33 +0200

    add seasonal-5

--- render_static.R
+++ render_static.R
@@ -13,8 +13,12 @@ while (TRUE) {
   start   <- Sys.time()
   glue::glue("Deck - Start - {start}") |> message()
   
+  # Read Decks
+  LoR.Deck    <- data.table::fread(file.path("C:","LlorR","data","raw","LoR_DECK.csv"),header = T,na.strings = c("",NA) )
   source(file.path("C:","LlorR","scripts","dataIO","lor_deck_v3.R"), encoding = "UTF-8", local = TRUE)
+  # LoR.Deck.RMD <- LoR.Deck
   data.table::fwrite(LoR.Deck, file.path("C:", "LlorR", "data", "raw", "LoR_DECK.csv"))
+  
   
   end     <- Sys.time()
   glue::glue("Deck - End   - {end} / Duration: {round(as.numeric(lubridate::as.duration(end-start), 'seconds'),2)}s") |> message()
@@ -38,24 +42,28 @@ while (TRUE) {
   
 }
 
-distill::create_post(
-  "THE META REPORT NAME IS TOO LONG, TOO DAMN LONG (n°XX)",
-  collection = "report",
-  author = "Valentino (Legna) Vazzoler",
-  slug = "meta-report-060",
-  date = "2021-05-11",
-  date_prefix = NULL,
-  draft = TRUE,
-  edit = interactive()
-)
 
-distill::create_post(
-  "THE SEASONAL REPORT IS VAST (n°XXX)",
-  collection = "report",
-  author = "Valentino (Legna) Vazzoler",
-  slug = "seasonal-005",
-  date = "2022-05-14",
-  date_prefix = NULL,
-  draft = FALSE,
-  edit = interactive()
-)
+
+# OlsonNames()
+
+# distill::create_post(
+#   "THE META REPORT NAME IS TOO LONG, TOO DAMN LONG (n°XX)",
+#   collection = "report",
+#   author = "Valentino (Legna) Vazzoler",
+#   slug = "meta-report-060",
+#   date = "2021-05-11",
+#   date_prefix = NULL,
+#   draft = TRUE,
+#   edit = interactive()
+# )
+# 
+# distill::create_post(
+#   "THE SEASONAL REPORT IS VAST (n°XXX)",
+#   collection = "report",
+#   author = "Valentino (Legna) Vazzoler",
+#   slug = "seasonal-005",
+#   date = "2022-05-14",
+#   date_prefix = NULL,
+#   draft = FALSE,
+#   edit = interactive()
+# )
