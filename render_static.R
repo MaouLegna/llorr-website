be6a223cd38916df5f173f6fe03acc15185d7188
MaouLegna <valentino.vazzoler.86@gmail.com>
2022-09-01 05:33:35 +0200

    add set6-3 2022-09-01 05:33

--- render_static.R
+++ render_static.R
@@ -2,12 +2,12 @@ source(file.path("C:","LlorR","scripts","lor_main.R" ))
 source(file.path("C:","LlorR","scripts","functions","lor_constants.R"))
 source(file.path("C:","LlorR","scripts","functions","lor_functions.R"))
 
-update_lor_database(server_abbr = "EU",last_season = "S14",delete = TRUE)
-update_lor_database(server_abbr = "NA",last_season = "S14",delete = TRUE)
-update_lor_database(server_abbr = "APAC",last_season = "S14",delete = TRUE)
+update_lor_database(server_abbr = "EU",last_season = "S15",delete = TRUE)
+update_lor_database(server_abbr = "NA",last_season = "S15",delete = TRUE)
+update_lor_database(server_abbr = "APAC",last_season = "S15",delete = TRUE)
 
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu.Rmd"),  output_file = "mu.html",   encoding = 'UTF-8', clean = TRUE)
-rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta.Rmd"),output_file = "meta.html", encoding = 'UTF-8', clean = TRUE)
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","mu_temp.Rmd"),  output_file = "mu.html",   encoding = 'UTF-8', clean = TRUE)
+rmarkdown::render(input = file.path("C:","Users","Valentino Vazzoler","Documents","R","llorr-website","static","meta_temp.Rmd"),output_file = "meta.html", encoding = 'UTF-8', clean = TRUE)
 rmarkdown::render_site(encoding = 'UTF-8')
 
 # git add -A && git commit -m "update 2022-08-28 10:40"
